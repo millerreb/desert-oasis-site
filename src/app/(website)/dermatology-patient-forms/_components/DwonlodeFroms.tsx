452777aca91e515e234aabe0ed69950dd2ffeaf9
millerreb <beemills@gmail.com>
2025-08-03 21:53:59 -0700

    download form working

--- src/app/(website)/dermatology-patient-forms/_components/DwonlodeFroms.tsx
+++ src/app/(website)/dermatology-patient-forms/_components/DwonlodeFroms.tsx
@@ -9,13 +9,7 @@ export default function DownloadForms() {
                 <span>
                     <HiOutlineDownload className="text-[#A66A47] w-5 h-5 md:w-7 md:h-7" />
                 </span>
-                <h3 className="text-[18px] md:test-[24px] text-[#A66A47] font-semibold underline">Post-Procedure Instructions</h3>
-            </div>
-            <div className="flex items-center gap-4 mt-[30px]">
-                <span>
-                    <HiOutlineDownload className="text-[#A66A47] w-5 h-5 md:w-7 md:h-7" />
-                </span>
-                <h3 className="text-[18px] md:test-[24px] text-[#A66A47] font-semibold underline">Post-Procedure Instructions</h3>
+                <a className="text-[18px] md:test-[24px] text-[#A66A47] font-semibold underline" download href="/documents/DesertOasis-After-Surgery-Care.pdf">Post-Procedure Instructions</a>
             </div>
         </div>
     )
--- src/app/(website)/dermatology-patient-forms/page.tsx
+++ src/app/(website)/dermatology-patient-forms/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Headers/Header";
 import React from "react";
 import DownloadForms from "./_components/DwonlodeFroms";
-import FromUplode from "./_components/FromUplode";
 import Consultation from "@/components/Consultation/Consultation";
 
 const page = () => {
@@ -12,9 +11,8 @@ const page = () => {
           title="Dermatology Patient Forms"
           subtitle="Download and submit the necessary patient forms to streamline your visit. Please download the required PDFs and upload your completed forms below."
               />
-              <div className=" container">
+              <div className="container py-[60px]">
                   <DownloadForms />
-                  <FromUplode />
               </div>
       </div>
       <Consultation/>
